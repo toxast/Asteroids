ff22599dad0d302eb2ebd037e84481f78ba1be34
gtoxast <gtoxast@gmail.com>
2014-03-18 17:30:21 +0400

    Folders

---
+++ Assets/Scripts/BulletData.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletData
+{
+	public Vector2 position;
+	public Vector2 direction;
+	public float speed;
+	public Vector2[] vertices; //TODO: share mesh
+	public float fireInterval;
+	public float travelDistance;
+
+
+}
