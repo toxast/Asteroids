d924e56872bb2a4719ae8bb06f7c6db6c5eb88a2
toxast <gtoxast@gmail.com>
2015-01-20 12:44:55 +0400

    j

---
+++ Assets/Scripts/FireButton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FireButton : Button {
+
+	public bool pressed = false;
+
+	public override void OnPointerDown (UnityEngine.EventSystems.PointerEventData eventData)
+	{
+		base.OnPointerDown (eventData);
+		pressed = true;
+	}
+
+	public override void OnPointerUp (UnityEngine.EventSystems.PointerEventData eventData)
+	{
+		base.OnPointerUp (eventData);
+		pressed = false;
+	}
+}
