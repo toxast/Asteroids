29601aafccd24ef2f44ee3ab3e528d20546a9027
toxast <gtoxast@gmail.com>
2016-12-22 14:39:32 +0400

    levels consistency check

--- Assets/Scripts/ResourceScripts/LevelsResources.cs
+++ Assets/Scripts/ResourceScripts/LevelsResources.cs
@@ -5,14 +5,21 @@ using System.Collections.Generic;
 public class LevelsResources : ResourceSingleton<LevelsResources> {
 	[SerializeField] public List<SpawnWaves> levels;
 
-//    [ContextMenu ("ReplaceDataWithPrefabs")]
-//    private void ReplaceDataWithPrefabs (){
-//        foreach (var lvl in levels) {
-//            foreach (var wave in lvl.list) {
-//                foreach (var obj in wave.objects) {
-//                    obj.PlacePrefab ();
-//                }
-//            }
-//        }
-//    }
+    [ContextMenu ("Check prefabs consistency")]
+    private void ReplaceDataWithPrefabs (){
+        bool errors = false;
+        foreach (var lvl in levels) {
+            foreach (var wave in lvl.list) {
+                foreach (var obj in wave.objects) {
+                    if (obj.prefab == null || !(obj.prefab is ISwanable)) {
+                        Debug.LogError ("LevelsResources: prefab " + lvl.name + " " + wave.name + " " + obj.name + " is bad");
+                        errors = true;
+                    }
+                }
+            }
+        }
+        if (!errors) {
+            Debug.Log("LevelsResources: everything is cool");
+        }
+    }
 }
