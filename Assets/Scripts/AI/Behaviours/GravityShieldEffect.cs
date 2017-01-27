6fa909380ea1b4d4511fe502c3b3b5de1e7b1aa5
toxast <gtoxast@gmail.com>
2017-01-27 21:17:40 +0300

    GravityShieldEffect setup

---
+++ Assets/Scripts/AI/Behaviours/GravityShieldEffect.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GravityShieldEffect : TickableEffect 
+{
+	protected override eType etype { get { return eType.GravityShield; } }
+	Data data;
+	float timeLeft;
+	float currentForce;
+	List<PolygonGameObject> gobjects;
+	List<PolygonGameObject> bullets;
+
+	public GravityShieldEffect(Data data) {
+		this.data = data;
+		timeLeft = data.duration;
+		currentForce = data.force;
+		gobjects = Singleton<Main>.inst.gObjects;
+		bullets = Singleton<Main>.inst.bullets;
+	}
+
+	public override void Tick (float delta) {
+		base.Tick (delta);
+
+		if (!IsFinished ()) {
+			timeLeft -= delta;
+			new GravityForceExplosion (holder.position, data.range, 0, delta * currentForce, gobjects, holder.collision);
+			new GravityForceExplosion (holder.position, data.range, 0, delta * currentForce, bullets, holder.collision);
+		}
+	}
+
+	public virtual bool IsFinished() {
+		return timeLeft <= 0;
+	}
+
+	/// <summary>
+	/// repalces current dps with the maximum dps, adjusts the duration so the total damage is preserved
+	/// duration is cut by the effect with the max dps
+	/// </summary>
+	public override void UpdateBy (TickableEffect sameEffect) {
+		base.UpdateBy (sameEffect);
+		var same = sameEffect as GravityShieldEffect;
+		timeLeft += same.data.duration;
+		currentForce = Mathf.Max (currentForce, same.data.force);
+	}
+
+	[System.Serializable]
+	public class Data{
+		public float duration = 30;
+		public float range = 20;
+		public float force = 20;
+		//TODO: add effect particle system here
+	}
+}
--- Assets/Scripts/PolygonGameObjects/ITickable.cs
+++ Assets/Scripts/PolygonGameObjects/ITickable.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public interface ITickable
 {
@@ -11,7 +12,7 @@ public abstract class TickableEffect : ITickable
 	protected abstract eType etype{ get;}
 	protected PolygonGameObject holder;
 
-	public void SetHolder(PolygonGameObject holder) {
+	public virtual void SetHolder(PolygonGameObject holder) {
 		this.holder = holder;
 	}
 	public virtual void Tick(float delta) { }
@@ -93,58 +94,3 @@ public class BurningEffect : DOTEffect {
 	protected override eType etype { get { return eType.Burning; } }
 	public BurningEffect (Data data) : base (data){}
 }
-
-//public class GravityShieldEffect : TickableEffect 
-//{
-//	override eType etype { get { return eType.GravityShield; } }
-//	Data data;
-//	float timeLeft;
-//	float currentDps;
-//
-//	public DOTEffect(Data data) {
-//		this.data = data;
-//		timeLeft = data.duration;
-//		currentDps = data.dps;
-//	}
-//
-//	public override void Tick (float delta) {
-//		base.Tick (delta);
-//		timeLeft -= delta;
-//	}
-//
-//	public virtual bool IsFinished() {
-//		return timeLeft <= 0;
-//	}
-//
-//	/// <summary>
-//	/// repalces current dps with the maximum dps, adjusts the duration so the total damage is preserved
-//	/// duration is cut by the effect with the max dps
-//	/// </summary>
-//	public override void UpdateBy (TickableEffect sameEffect) {
-//		base.UpdateBy (sameEffect);
-//		var same = sameEffect as DOTEffect;
-//
-//		float maxDps;
-//		float maxDuration;
-//		if (currentDps > same.data.dps) {
-//			maxDps = currentDps;
-//			maxDuration = data.maxBuildUpDuration;
-//		} else {
-//			maxDps = same.data.dps;
-//			maxDuration = same.data.maxBuildUpDuration;
-//		}
-//
-//		float totalDamageToBeDone = timeLeft * currentDps + same.data.dps * same.data.duration;
-//
-//		currentDps = maxDps;
-//		timeLeft = Mathf.Min(totalDamageToBeDone / maxDps, maxDuration);
-//	}
-//
-//	[System.Serializable]
-//	public class Data{
-//		public float duration = 1;
-//		public float dps = 3;
-//		public float maxBuildUpDuration = 5;
-//		//TODO: add effect particle system here
-//	}
-//}
